f0cc6381b6bbba75f376d70426141fdb17427f0f
rajashekhar-a <rajuavunuri123@gmail.com>
2024-07-09 23:24:48 +0530

    update changes

--- vpc/subnets.tf
+++ vpc/subnets.tf
@@ -1,4 +1,5 @@
 resource "aws_subnet" "private-subnets" {
+  depends_on = [aws_vpc_ipv4_cidr_block_association.addon]
   count      = length(var.PRIVATE_SUBNETS)
   vpc_id     = aws_vpc.main.id
   cidr_block = element(var.PRIVATE_SUBNETS, count.index)
@@ -10,6 +11,7 @@ resource "aws_subnet" "private-subnets" {
 }
 
 resource "aws_subnet" "public-subnets" {
+  depends_on = [aws_vpc_ipv4_cidr_block_association.addon]
   count      = length(var.PUBLIC_SUBNETS)
   vpc_id     = aws_vpc.main.id
   cidr_block = element(var.PUBLIC_SUBNETS, count.index)
