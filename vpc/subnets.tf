b3036f736c73c5754d80e6bdf9a1bd47750c83d7
rajashekhar-a <rajuavunuri123@gmail.com>
2024-07-09 23:16:11 +0530

    create nat gateway

--- vpc/subnets.tf
+++ vpc/subnets.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "private" {
+resource "aws_subnet" "private-subnets" {
   count      = length(var.PRIVATE_SUBNETS)
   vpc_id     = aws_vpc.main.id
   cidr_block = element(var.PRIVATE_SUBNETS, count.index)
@@ -9,7 +9,7 @@ resource "aws_subnet" "private" {
   }
 }
 
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public-subnets" {
   count      = length(var.PUBLIC_SUBNETS)
   vpc_id     = aws_vpc.main.id
   cidr_block = element(var.PUBLIC_SUBNETS, count.index)
