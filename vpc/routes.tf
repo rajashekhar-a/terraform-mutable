5e4427adb3c6f3e763de79bda46570c039557b13
rajashekhar-a <rajuavunuri123@gmail.com>
2024-07-12 17:20:53 +0530

    update the changes for the route table

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -5,17 +5,6 @@ resource "aws_route_table" "private-route" {
     {
       cidr_block                   = "0.0.0.0/0"
       nat_gateway_id               = aws_nat_gateway.ngw.id
-      "carrier_gateway_id"         = ""
-      "core_network_arn"           = ""
-      "destination_prefix_list_id" = ""
-      "egress_only_gateway_id"     = ""
-      "gateway_id"                 = ""
-      "ipv6_cidr_block"            = ""
-      "local_gateway_id"           = ""
-      "network_interface_id"       = ""
-      "transit_gateway_id"         = ""
-      "vpc_endpoint_id"            = ""
-      "vpc_peering_connection_id"  = ""
     },
     {
       cidr_block                   = var.DEFAULT_VPC_CIDR
@@ -81,19 +70,7 @@ resource "aws_route_table" "public-route" {
   route = [
     {
       cidr_block                   = "0.0.0.0/0"
-      "nat_gateway_id"             = ""
-      "carrier_gateway_id"         = ""
-      "core_network_arn"           = ""
-      "destination_prefix_list_id" = ""
-      "egress_only_gateway_id"     = ""
       "gateway_id"                 = aws_internet_gateway.igw.id
-      "ipv6_cidr_block"            = ""
-      "local_gateway_id"           = ""
-      "network_interface_id"       = ""
-      "transit_gateway_id"         = ""
-      "vpc_endpoint_id"            = ""
-      "vpc_peering_connection_id"  = ""
-
     },
     {
       cidr_block                   = var.DEFAULT_VPC_CIDR
