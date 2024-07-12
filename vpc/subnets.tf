b53b0fcddb639ee00f1b8c1c204ee4ddbeeb6550
rajashekhar-a <rajuavunuri123@gmail.com>
2024-07-12 16:25:26 +0530

    associate route table to subnets

--- vpc/subnets.tf
+++ vpc/subnets.tf
@@ -1,8 +1,8 @@
 resource "aws_subnet" "private-subnets" {
-  depends_on = [aws_vpc_ipv4_cidr_block_association.addon]
-  count      = length(var.PRIVATE_SUBNETS)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.PRIVATE_SUBNETS, count.index)
+  depends_on        = [aws_vpc_ipv4_cidr_block_association.addon]
+  count             = length(var.PRIVATE_SUBNETS)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.PRIVATE_SUBNETS, count.index)
   availability_zone = element(var.AZS, count.index)
 
   tags = {
@@ -11,13 +11,25 @@ resource "aws_subnet" "private-subnets" {
 }
 
 resource "aws_subnet" "public-subnets" {
-  depends_on = [aws_vpc_ipv4_cidr_block_association.addon]
-  count      = length(var.PUBLIC_SUBNETS)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.PUBLIC_SUBNETS, count.index)
+  depends_on        = [aws_vpc_ipv4_cidr_block_association.addon]
+  count             = length(var.PUBLIC_SUBNETS)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.PUBLIC_SUBNETS, count.index)
   availability_zone = element(var.AZS, count.index)
 
   tags = {
     Name = "public-subnet-${count.index}"
   }
+}
+
+resource "aws_route_table_association" "priv-assoc" {
+  count          = length(aws_subnet.private-subnets.*.id)
+  subnet_id      = element(aws_subnet.private-subnets.*.id, count.index)
+  route_table_id = aws_route_table.private-route.id
+}
+
+resource "aws_route_table_association" "priv-assoc" {
+  count          = length(aws_subnet.private-subnets.*.id)
+  subnet_id      = element(aws_subnet.public-subnets.*.id, count.index )
+  route_table_id = aws_route_table.public-route.id 
 }
