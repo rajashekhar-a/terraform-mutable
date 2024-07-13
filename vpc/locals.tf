ea67f7f73c9238530a36660e0b2c20e678e35ba5
rajashekhar-a <rajuavunuri123@gmail.com>
2024-07-14 00:00:25 +0530

    update changes

--- vpc/locals.tf
+++ vpc/locals.tf
@@ -1,4 +1,3 @@
 locals {
-  VPC_CIDR_MAIN = split(",", var.VPC_CIDR_MAIN)
-  ALL_VPC_CIDR  = concat(local.VPC_CIDR_MAIN, var.VPC_CIDR_ADDON)
+  ALL_VPC_CIDR = split(",", var.VPC_CIDR_MAIN, var.VPC_CIDR_ADDON)
 }
