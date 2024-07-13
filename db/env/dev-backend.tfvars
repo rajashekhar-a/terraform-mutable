538f4bbbbf4dd1e4688e042f1c6bd1c19be1c5d4
rajashekhar-a <rajuavunuri123@gmail.com>
2024-07-13 23:54:46 +0530

    create mysql db

---
+++ db/env/dev-backend.tfvars
@@ -0,0 +1,3 @@
+bucket = "terraform-mutable"
+key    = "terraform-mutable/db/dev/terraform.tfstate"
+region = "us-east-1"
---
+++ db/mysql-rds.tf
@@ -0,0 +1,54 @@
+resource "aws_db_instance" "mysql" {
+  allocated_storage    = 10
+  db_name              = "mysql-${ENV}"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "admin"
+  password             = "admin123"
+  parameter_group_name = aws_db_parameter_group.pg.name
+  skip_final_snapshot  = true
+}
+
+resource "aws_db_parameter_group" "pg" {
+  name   = "mysql-${ENV}_pg"
+  family = "mysql5.7"
+}
+
+resource "aws_security_group" "mysql" {
+  name        = "mysql-${var.ENV}"
+  description = "mysql-${var.ENV}"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress = [
+    {
+      description      = "MYSQL"
+      from_port        = 3306
+      to_port          = 3306
+      protocol         = "tcp"
+      cidr_blocks      = ["10.0.0.0/16"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  egress = [
+    {
+      description      = "egress"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  tags = {
+    Name = "mysql-${var.ENV}"
+  }
+}
