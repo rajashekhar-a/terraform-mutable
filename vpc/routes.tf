fcb965124f9bcf970ccea08248e7aab8cb45d9e1
rajashekhar-a <rajuavunuri123@gmail.com>
2024-07-13 20:06:53 +0530

    make peering connection

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -23,6 +23,30 @@ resource "aws_route_table" "public-route" {
   }
 }
 
+resource "aws_route" "private_route_to_main_route" {
+  route_table_id = aws_route_table.private-route.id
+  destination_cidr_block = var.DEFAULT_VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
+
+resource "aws_route" "public_route_to_main_route" {
+  route_table_id = aws_route_table.public-route.id
+  destination_cidr_block = var.DEFAULT_VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
+
+resource "aws_route" "main_route_to_private_route" {
+  route_table_id = var.DEFAULT_VPC_ROUTE_TABLE_ID
+  destination_cidr_block = var.VPC_CIDR_MAIN
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
+
+resource "aws_route" "main_route_to_public_route" {
+  route_table_id = var.DEFAULT_VPC_ROUTE_TABLE_ID
+  destination_cidr_block = var.VPC_CIDR_ADDON
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
+
 #resource "aws_route" "route-from-default-vpc" {
 #  count                     = length(local.association-list)
 #  route_table_id            = tomap(element(local.association-list, count.index))["route_table"]
