335f1983df899930acc4d148f7225a059a77e54a
rajashekhar-a <rajuavunuri123@gmail.com>
2024-07-13 18:54:16 +0530

    update the changes

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -1,41 +1,11 @@
 resource "aws_route_table" "private-route" {
   vpc_id = aws_vpc.main.id
 
-  route = [
-    {
-      cidr_block                   = var.DEFAULT_VPC_CIDR
-      vpc_peering_connection_id    = aws_vpc_peering_connection.peer.id
-      carrier_gateway_id           = ""
-      "destination_prefix_list_id" = ""
-      "egress_only_gateway_id"     = ""
-      "gateway_id"                 = ""
-      "instance_id"                = ""
-      "ipv6_cidr_block"            = ""
-      "local_gateway_id"           = ""
-      "nat_gateway_id"             = ""
-      "network_interface_id"       = ""
-      "transit_gateway_id"         = ""
-      "vpc_endpoint_id"            = ""
-      "core_network_arn"           = ""
-    },
-    {
-      cidr_block                   = "0.0.0.0/0"
-      vpc_peering_connection_id    = ""
-      carrier_gateway_id           = ""
-      "destination_prefix_list_id" = ""
-      "egress_only_gateway_id"     = ""
-      "gateway_id"                 = ""
-      "instance_id"                = ""
-      "ipv6_cidr_block"            = ""
-      "local_gateway_id"           = ""
-      "nat_gateway_id"             = aws_nat_gateway.ngw.id
-      "network_interface_id"       = ""
-      "transit_gateway_id"         = ""
-      "vpc_endpoint_id"            = ""
-      "core_network_arn"           = ""
-    }
-  ]
-
+  route
+  {
+    cidr_block       = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
   tags = {
     Name = "private-route"
   }
@@ -44,50 +14,21 @@ resource "aws_route_table" "private-route" {
 resource "aws_route_table" "public-route" {
   vpc_id = aws_vpc.main.id
 
-  route = [
-    {
-      cidr_block                   = var.DEFAULT_VPC_CIDR
-      vpc_peering_connection_id    = aws_vpc_peering_connection.peer.id
-      carrier_gateway_id           = ""
-      "destination_prefix_list_id" = ""
-      "egress_only_gateway_id"     = ""
-      "gateway_id"                 = ""
-      "instance_id"                = ""
-      "ipv6_cidr_block"            = ""
-      "local_gateway_id"           = ""
-      "nat_gateway_id"             = ""
-      "network_interface_id"       = ""
-      "transit_gateway_id"         = ""
-      "vpc_endpoint_id"            = ""
-      "core_network_arn"           = ""
-    },
+  route
     {
       cidr_block                   = "0.0.0.0/0"
-      vpc_peering_connection_id    = ""
-      carrier_gateway_id           = ""
-      "destination_prefix_list_id" = ""
-      "egress_only_gateway_id"     = ""
-      "gateway_id"                 = aws_internet_gateway.igw.id
-      "instance_id"                = ""
-      "ipv6_cidr_block"            = ""
-      "local_gateway_id"           = ""
-      "nat_gateway_id"             = ""
-      "network_interface_id"       = ""
-      "transit_gateway_id"         = ""
-      "vpc_endpoint_id"            = ""
-      "core_network_arn"           = ""
+      gateway_id                 = aws_internet_gateway.igw.id
     }
-  ]
 
   tags = {
     Name = "public-route"
   }
 }
 
-resource "aws_route" "route-from-default-vpc" {
-  count                     = length(local.association-list)
-  route_table_id            = tomap(element(local.association-list, count.index))["route_table"]
-  destination_cidr_block    = tomap(element(local.association-list, count.index))["cidr"]
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
+#resource "aws_route" "route-from-default-vpc" {
+#  count                     = length(local.association-list)
+#  route_table_id            = tomap(element(local.association-list, count.index))["route_table"]
+#  destination_cidr_block    = tomap(element(local.association-list, count.index))["cidr"]
+#  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+#}
 
