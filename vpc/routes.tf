a4da71e012d12ecd975d543c1e10661489ec8f78
rajashekhar-a <rajuavunuri123@gmail.com>
2024-07-13 19:13:09 +0530

    update the changes

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -1,8 +1,8 @@
 resource "aws_route_table" "private-route" {
   vpc_id = aws_vpc.main.id
 
-  route = {
-    cidr_block       = "0.0.0.0/0"
+  route {
+    cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.ngw.id
   }
   tags = {
@@ -13,9 +13,9 @@ resource "aws_route_table" "private-route" {
 resource "aws_route_table" "public-route" {
   vpc_id = aws_vpc.main.id
 
-  route = {
-      cidr_block                   = "0.0.0.0/0"
-      gateway_id                 = aws_internet_gateway.igw.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
 
   tags = {
