1f615de6fc4b3c9ac0fb5865a4ca7f3e7f0803a0
rajashekhar-a <rajuavunuri123@gmail.com>
2024-12-14 23:07:59 +0530

    update bucket name in data.tf

--- db/data.tf
+++ db/data.tf
@@ -1,8 +1,8 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "terraform-mutable"
-    key    = "terraform-mutable/vpc/dev/terraform.tfstate"
+    bucket = "terraform-mutable1"
+    key    = "terraform-mutable1/vpc/dev/terraform.tfstate"
     region = "us-east-1"
   }
 }
