78c574d6e274d2dec2d935b2beb740e748bcc60b
rajashekhar-a <rajuavunuri123@gmail.com>
2024-12-14 23:13:27 +0530

    update db name

--- db/mysql-rds.tf
+++ db/mysql-rds.tf
@@ -1,6 +1,6 @@
 resource "aws_db_instance" "mysql" {
   allocated_storage    = 10
-  db_name              = "mysql-dev"
+  db_name              = "devdbmysql"
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
