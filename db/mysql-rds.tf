216c618780b7572fcc31e95c0282d59f652d8d35
rajashekhar-a <rajuavunuri123@gmail.com>
2024-12-16 23:12:41 +0530

    load schema

--- db/mysql-rds.tf
+++ db/mysql-rds.tf
@@ -70,3 +70,16 @@ resource "aws_route53_record" "mysql" {
   ttl     = "300"
   records = [aws_db_instance.mysql.address]
 }
+
+resource "null_resource" "schema-apply" {
+  provisioner "local-exec" {
+    command = <<EOF
+    sudo yum install mariadb -y
+    curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+    cd /tmp
+    unzip -o /tmp/mysql.zip
+    mysql -h${aws_db_instance.mysql.address} -uadmin -padmin123 <mysql-main/shipping.sql
+    EOF
+
+  }
+}
