a1c6df5406557ef5fd5de8fcc77ad3c3277931db
rajashekhar-a <rajuavunuri123@gmail.com>
2024-12-16 22:10:41 +0530

    add routes from default vpc

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -2,7 +2,7 @@ resource "aws_route_table" "private-route" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.ngw.id
   }
   tags = {
@@ -23,34 +23,34 @@ resource "aws_route_table" "public-route" {
   }
 }
 
-resource "aws_route" "private_route_to_main_route" {
-  route_table_id = aws_route_table.private-route.id
-  destination_cidr_block = var.DEFAULT_VPC_CIDR
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
-
-resource "aws_route" "public_route_to_main_route" {
-  route_table_id = aws_route_table.public-route.id
-  destination_cidr_block = var.DEFAULT_VPC_CIDR
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
-
-resource "aws_route" "main_route_to_private_route" {
-  route_table_id = var.DEFAULT_VPC_ROUTE_TABLE_ID
-  destination_cidr_block = var.VPC_CIDR_MAIN
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
-
-resource "aws_route" "main_route_to_public_route" {
-  route_table_id = var.DEFAULT_VPC_ROUTE_TABLE_ID
-  destination_cidr_block = var.VPC_CIDR_ADDON
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
-
-#resource "aws_route" "route-from-default-vpc" {
-#  count                     = length(local.association-list)
-#  route_table_id            = tomap(element(local.association-list, count.index))["route_table"]
-#  destination_cidr_block    = tomap(element(local.association-list, count.index))["cidr"]
+#resource "aws_route" "private_route_to_main_route" {
+#  route_table_id            = aws_route_table.private-route.id
+#  destination_cidr_block    = var.DEFAULT_VPC_CIDR
 #  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 #}
+#
+#resource "aws_route" "public_route_to_main_route" {
+#  route_table_id            = aws_route_table.public-route.id
+#  destination_cidr_block    = var.DEFAULT_VPC_CIDR
+#  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+#}
+#
+#resource "aws_route" "main_route_to_private_route" {
+#  route_table_id            = var.DEFAULT_VPC_ROUTE_TABLE_ID
+#  destination_cidr_block    = var.VPC_CIDR_MAIN
+#  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+#}
+#
+#resource "aws_route" "main_route_to_public_route" {
+#  route_table_id            = var.DEFAULT_VPC_ROUTE_TABLE_ID
+#  destination_cidr_block    = var.VPC_CIDR_ADDON
+#  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+#}
+
+resource "aws_route" "route-from-default-vpc" {
+  count                     = length(local.association-list)
+  route_table_id            = tomap(element(local.association-list, count.index))["route_table"]
+  destination_cidr_block    = tomap(element(local.association-list, count.index))["cidr"]
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
 
