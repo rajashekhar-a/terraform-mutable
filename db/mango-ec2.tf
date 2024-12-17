c1c1b56611652aa29c229de32121864c723946af
rajashekhar-a <rajuavunuri123@gmail.com>
2024-12-17 15:02:26 +0530

    edit inbound rules in sg

--- db/mango-ec2.tf
+++ db/mango-ec2.tf
@@ -32,6 +32,17 @@ resource "aws_security_group" "mongo" {
       prefix_list_ids  = []
       security_groups  = []
       self             = false
+    },
+    {
+      description      = "SSH"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
     }
   ]
 
