3f1e051c8204913e27d01c7eaef35257bc5a04a4
rajashekhar-a <rajuavunuri123@gmail.com>
2024-12-18 21:34:11 +0530

    update changes

--- app-module/ec2.tf
+++ app-module/ec2.tf
@@ -1,16 +1,18 @@
 resource "aws_spot_instance_request" "spot-instance" {
-  ami           = "ami-1234"
-  instance_type = var.SPOT_INSTANCE_TYPE
+  count                = var.SPOT_INSTANCE_COUNT
+  ami                  = data.aws_ami.ami.id
+  instance_type        = var.SPOT_INSTANCE_TYPE
   wait_for_fulfillment = true
-  subnet_id = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS, count.index)
+  subnet_id            = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS, count.index)
 
-   tags = {
-    Name = "CheapWorker"
+  tags = {
+    Name = "sample"
   }
 }
 
 resource "aws_instance" "od" {
-  ami                    = data.aws_ami.ami.id
-  instance_type          = var.INSTANCE_TYPE
-  subnet_id              = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS, count.index)
+  count = var.OD_INSTANCE_COUNT
+  ami           = data.aws_ami.ami.id
+  instance_type = var.INSTANCE_TYPE
+  subnet_id     = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS, count.index)
 }
