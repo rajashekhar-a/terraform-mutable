a11e6feacbb8130de4df33e083bc3f85905da35e
rajashekhar-a <rajuavunuri123@gmail.com>
2024-12-18 17:44:01 +0530

    add vpc addon list

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -7,6 +7,7 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_vpc_ipv4_cidr_block_association" "addon" {
+  count      = length(var.VPC_CIDR_ADDON)
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.VPC_CIDR_ADDON
+  cidr_block = element(var.VPC_CIDR_ADDON, count.index)
 }
