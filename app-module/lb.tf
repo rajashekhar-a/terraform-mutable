2cb4d6e47ecd52f67ba16f37c8e47589eb142954
rajashekhar-a <rajuavunuri123@gmail.com>
2024-12-22 00:48:57 +0530

    add listener

--- app-module/lb.tf
+++ app-module/lb.tf
@@ -12,33 +12,18 @@ resource "aws_lb_target_group_attachment" "tg-attach" {
   port             = var.PORT
 }
 
-resource "aws_lb_listener" "private-listener" {
-  load_balancer_arn = data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ARN
-  port              = var.PORT
-  protocol          = "HTTP"
+resource "aws_lb_listener_rule" "private-listener-rule" {
+  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
+  priority     = 100
 
-  default_action {
-    type = "redirect"
-
-    redirect {
-      port        = var.PORT
-      protocol    = "HTTP"
-      status_code = "200"
-    }
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
   }
-}
-resource "aws_lb_listener" "private_listener" {
-  load_balancer_arn = data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ARN
-  port              = "80"
-  protocol          = "HTTP"
 
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "OK"
-      status_code  = "200"
+  condition {
+    host_header {
+      values = ["${var.COMPONENT}-${var.ENV}.roboshop.internal"]
     }
   }
 }
--- app-module/vars.tf
+++ app-module/vars.tf
@@ -5,3 +5,4 @@ variable "SPOT_INSTANCE_COUNT" {}
 variable "OD_INSTANCE_COUNT" {}
 variable "COMPONENT" {}
 variable "PORT" {}
+variable "IS_PRIVATE_LB" {}
