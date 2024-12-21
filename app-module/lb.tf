8e3bfe31331d36a8ca3a32533072d41bbd78707d
rajashekhar-a <rajuavunuri123@gmail.com>
2024-12-22 01:22:46 +0530

    update changes

--- app-module/lb.tf
+++ app-module/lb.tf
@@ -8,7 +8,7 @@ resource "aws_lb_target_group" "tg" {
 resource "aws_lb_target_group_attachment" "tg-attach" {
   count            = length(local.INSTANCE_IDS)
   target_group_arn = aws_lb_target_group.tg.arn
-  target_id        = element(local.PRIVATE_IPS, count.index)
+  target_id        = element(local.INSTANCE_IDS, count.index)
   port             = var.PORT
 }
 
