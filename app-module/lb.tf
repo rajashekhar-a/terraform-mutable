8b3386315c3d24859ce8037755a4284c3cce9927
rajashekhar-a <rajuavunuri123@gmail.com>
2024-12-23 22:14:11 +0530

    update change

--- app-module/lb.tf
+++ app-module/lb.tf
@@ -13,7 +13,7 @@ resource "aws_lb_target_group_attachment" "tg-attach" {
 }
 
 resource "aws_lb_listener_rule" "private-listener-rule" {
-  IS_PRIVATE_LB = true ? 1 : 0
+  count         = var.IS_PRIVATE_LB ? 1 : 0
   listener_arn  = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
   priority      = 100
 
