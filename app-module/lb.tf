b38480ecdcfa3c6e5e33b80997e155e69f0bc402
rajashekhar-a <rajuavunuri123@gmail.com>
2024-12-23 21:45:30 +0530

    update listerner rule

--- app-module/lb.tf
+++ app-module/lb.tf
@@ -13,8 +13,9 @@ resource "aws_lb_target_group_attachment" "tg-attach" {
 }
 
 resource "aws_lb_listener_rule" "private-listener-rule" {
-  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
-  priority     = 100
+  IS_PRIVATE_LB = true ? 1 : 0
+  listener_arn  = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
+  priority      = 100
 
   action {
     type             = "forward"
